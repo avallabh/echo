7269201d80e455fee2a8d9270309c22e98353ee9
Ajay Vallabh <ajay.vallabh@mail.com>
2013-11-24 11:09:34 -0500

    Cosmetic p3 changes on puts statements

--- echo3.rb
+++ echo3.rb
@@ -1,5 +1,4 @@
 def playback(input)
-
   puts input
   input = gets.chomp
   if input == 'Nothing!'
@@ -8,23 +7,21 @@ def playback(input)
   end
   if input == 'I have a lot to say'
     array = []
-    puts "Okay, what do you have to say? "
+    puts 'Okay, what do you have to say? '
     while true
       input = gets.chomp
       array << input
-      if input == "done"
-        exit
-        # giant ass loop
+      if input == 'done'
         array.pop
         length = array.length
         array2 = array.shift
         array3 = array.pop
-        puts "First you said #{array2}"
+        puts "You said #{array2}"
         array.each do |i|
           puts "Then you said #{i}"
         end
         puts "Finally you said #{array3}"
-        puts "Phew, glad you got all #{length} of those things off your chest!"
+        puts "Phew, glad you got those #{length} things off your chest!"
         break
       end
     end
@@ -33,5 +30,4 @@ def playback(input)
   end
 end
 
-
-playback("What do you want to say?: ")
+playback('What do you want to say?: ')
