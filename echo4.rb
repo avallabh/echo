911ee93f4a4149d1c00baaa54c8984056eeb3dc0
Ajay Vallabh <ajay.vallabh@mail.com>
2013-11-24 14:22:26 -0500

    Phase 4

--- echo4.rb
+++ echo4.rb
@@ -1,17 +1,3 @@
-# What do you want to say?
-# > I have something prepared
-# Ok, where can I find what you want to say?
-# > bad_file.txt
-# I couldn't find that file.
-# Where can I find what you want to say?
-# > speech.txt
-# Loading speech.txt...
-# You said: We're leaving ground
-# Then, you said: Will things ever be the same again?
-# Then, you said: It's the final countdown
-# Finally you said: The final countdown
-# Phew! Glad you got all 4 of those things off your chest!
-
 def playback(input)
   puts input
   input = gets.chomp
@@ -20,13 +6,29 @@ def playback(input)
     exit
   end
   if input == 'I have something prepared'
-    puts 'Okay, where can I find what you want to say? '
-    # specify filename
-    # if it doesn't exist, puts "I couldn't find that file"
-    # puts "where can I find what you want to say?"
-    # user puts valid filename
-    # Loading <file>...
-    #
+    array = []
+    while true
+      puts 'Where can I find what you want to say? '
+      input = gets.chomp
+      if File.exists?(input) == false
+        puts "I couldn't find that file."
+      else
+        puts "Loading #{input}..."
+        File.open("#{input}", "r").each_line do |line|
+          array << line
+        end
+        length = array.length
+        array_first = array.shift
+        array_last = array.pop
+        puts "You said: #{array_first}"
+        array.each do |i|
+          puts "Then you said: #{i}"
+        end
+        puts "Finally you said: #{array_last}"
+        puts "Phew, glad you got those #{length} things off your chest!"
+        exit
+      end
+    end
   end
   if input == 'I have a lot to say'
     array = []
@@ -37,15 +39,15 @@ def playback(input)
       if input == 'done'
         array.pop
         length = array.length
-        array2 = array.shift
-        array3 = array.pop
-        puts "You said #{array2}"
+        array_first = array.shift
+        array_last = array.pop
+        puts "You said: #{array_first}"
         array.each do |i|
-          puts "Then you said #{i}"
+          puts "Then you said: #{i}"
         end
-        puts "Finally you said #{array3}"
+        puts "Finally you said: #{array_last}"
         puts "Phew, glad you got those #{length} things off your chest!"
-        break
+        exit
       end
     end
   else
